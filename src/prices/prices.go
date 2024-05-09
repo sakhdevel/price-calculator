34021c65976c77f2624a7ed6e4b17236e9e4912d
sakhdevel <sakhdevel@yandex.ru>
2024-05-09 11:45:08 +0400

    Loading data from a file (beginning)

--- src/prices/prices.go
+++ src/prices/prices.go
@@ -1,11 +1,40 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+	if err != nil {
+		fmt.Println("Couldn't open file!")
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	var lines []string
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+	if err != nil {
+		fmt.Println("Reading file content failed!")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
 }
 
 func (job TaxIncludedPriceJob) Process() {
