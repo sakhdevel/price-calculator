4a887ac3281d2f318e16cd675fccffa25783b56d
sakhdevel <sakhdevel@yandex.ru>
2024-05-10 12:12:17 +0400

    Outsourcing Sharable Logic Into A Package

--- src/prices/prices.go
+++ src/prices/prices.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
+	"sakhdevel/price-calculator/conversion"
 )
 
 type TaxIncludedPriceJob struct {
@@ -36,21 +36,16 @@ func (job *TaxIncludedPriceJob) LoadData() {
 		return
 	}
 
-	prices := make([]float64, len(lines))
+	prices, err := conversion.StringsToFloats(lines)
 
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-		if err != nil {
-			fmt.Println("Converting price to float failed!")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-
-		prices[lineIndex] = floatPrice
+	if err != nil {
+		fmt.Println(err)
+		file.Close()
+		return
 	}
 
 	job.InputPrices = prices
+	file.Close()
 }
 
 func (job *TaxIncludedPriceJob) Process() {
